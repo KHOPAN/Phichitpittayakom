803515fc48b7208d0d46271342a60c1c6b6e2884
KHOPAN <93379212+KHOPAN@users.noreply.github.com>
2023-10-10 01:10:12 +0700

    Add Search Field
    Add search field in 'SearchStudentActivity.java'

--- phichitpittayakom/src/main/java/org/punlabs/phichitpittayakom/activity/SearchStudentActivity.java
+++ phichitpittayakom/src/main/java/org/punlabs/phichitpittayakom/activity/SearchStudentActivity.java
@@ -2,12 +2,23 @@ package org.punlabs.phichitpittayakom.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.Gravity;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.core.widget.NestedScrollView;
 
 import com.khopan.api.common.utils.ActivityUtils;
+import com.khopan.api.common.utils.LayoutUtils;
 import com.sec.sesl.org.punlabs.phichitpittayakom.R;
+
+import dev.oneuiproject.oneui.widget.RoundLinearLayout;
+import dev.oneuiproject.oneui.widget.Separator;
 
 public class SearchStudentActivity extends AppCompatActivity {
 	@Override
@@ -36,6 +47,51 @@ public class SearchStudentActivity extends AppCompatActivity {
 		}
 
 		String title = this.getString(resource);
-		LinearLayout linearLayout = ActivityUtils.initializeView(this, title, title, this.getString(R.string.collapseTitle), true);
+		FrameLayout frameLayout = ActivityUtils.initializeForFragment(this, title, title, this.getString(R.string.collapseTitle), true);
+		NestedScrollView scrollView = new NestedScrollView(this);
+		scrollView.setLayoutParams(new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+		scrollView.setFillViewport(true);
+		scrollView.setOverScrollMode(NestedScrollView.OVER_SCROLL_ALWAYS);
+		LayoutUtils.forceEnableScrollbar(scrollView, true, false);
+		frameLayout.addView(scrollView);
+		LinearLayout linearLayout = new LinearLayout(this);
+		linearLayout.setOrientation(LinearLayout.VERTICAL);
+		scrollView.addView(linearLayout);
+		Separator separator = new Separator(this);
+		String search = this.getString(R.string.search);
+		separator.setText(search);
+		linearLayout.addView(separator);
+		RoundLinearLayout searchLayout = new RoundLinearLayout(this);
+		LayoutUtils.setLayoutTransition(searchLayout);
+		searchLayout.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+		searchLayout.setOrientation(RoundLinearLayout.VERTICAL);
+		searchLayout.setBackgroundColor(this.getColor(R.color.oui_background_color));
+		linearLayout.addView(searchLayout);
+		LinearLayout searchView = new LinearLayout(this);
+		LayoutUtils.setLayoutTransition(searchView);
+		LinearLayout.LayoutParams searchViewParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+		int margin = Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 14.0f, this.getResources().getDisplayMetrics()));
+		searchViewParams.leftMargin = margin;
+		searchViewParams.topMargin = margin;
+		searchViewParams.rightMargin = margin;
+		searchViewParams.bottomMargin = margin;
+		searchViewParams.gravity = Gravity.CENTER;
+		searchView.setLayoutParams(searchViewParams);
+		searchView.setOrientation(LinearLayout.HORIZONTAL);
+		searchView.setGravity(Gravity.CENTER);
+		searchLayout.addView(searchView);
+		AppCompatButton searchButton = new AppCompatButton(this);
+		searchButton.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+		searchButton.setText(search);
+		searchButton.setFocusable(true);
+		searchButton.setClickable(true);
+		searchButton.setOnClickListener(view -> {});
+		searchView.addView(searchButton);
+		EditText searchField = new EditText(this);
+		LinearLayout.LayoutParams searchFieldParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+		searchFieldParams.weight = 1;
+		searchFieldParams.rightMargin = margin;
+		searchField.setLayoutParams(searchFieldParams);
+		searchView.addView(searchField, 0);
 	}
 }
