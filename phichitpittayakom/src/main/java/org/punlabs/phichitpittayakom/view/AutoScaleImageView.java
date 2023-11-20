214c3afbc4d1595543fce3a6802d2b4691f9a84e
KHOPAN <93379212+KHOPAN@users.noreply.github.com>
2023-11-20 22:31:30 +0700

    Add Set Image Functionality to AutoScaleImageView

--- phichitpittayakom/src/main/java/org/punlabs/phichitpittayakom/view/AutoScaleImageView.java
+++ phichitpittayakom/src/main/java/org/punlabs/phichitpittayakom/view/AutoScaleImageView.java
@@ -7,23 +7,35 @@ import android.graphics.Paint;
 import android.view.View;
 
 public class AutoScaleImageView extends View {
-	private final Bitmap image;
-	private final double aspectRatio;
 	private final Paint paint;
 
+	private Bitmap image;
+	private double aspectRatio;
 	private Bitmap scaledImage;
 
 	public AutoScaleImageView(Context context) {
 		super(context);
-		throw new IllegalArgumentException("Image missing!");
+		this.paint = new Paint();
+		this.aspectRatio = 1.0d;
 	}
 
 	public AutoScaleImageView(Context context, Bitmap image) {
-		super(context);
+		this(context);
+		this.setImage(image);
+	}
+
+	public void setImage(Bitmap image) {
 		this.image = image;
 		this.aspectRatio = ((double) this.image.getHeight()) / ((double) this.image.getWidth());
-		this.paint = new Paint();
 		this.scaledImage = this.image;
+		int width = this.getWidth();
+		int height = this.getHeight();
+
+		if(width < 1 || height < 1) {
+			return;
+		}
+
+		this.updateScaledImage(width, height);
 	}
 
 	@Override
@@ -36,6 +48,10 @@ public class AutoScaleImageView extends View {
 	@Override
 	protected void onSizeChanged(int width, int height, int oldWidth, int oldHeight) {
 		super.onSizeChanged(width, height, oldWidth, oldHeight);
+		this.updateScaledImage(width, height);
+	}
+
+	private void updateScaledImage(int width, int height) {
 		this.scaledImage = Bitmap.createScaledBitmap(this.image, width, height, true);
 	}
 
